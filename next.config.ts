066c3775d2e8d4c6cc99b62ce271d228b6ff6921
lodgepolepines <53026628+lodgepolepines@users.noreply.github.com>
2025-01-15 15:42:28 -0500

    add

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   output: 'export', // Enable static export
   trailingSlash: true, // Helps with GitHub Pages routing
-  
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },  
   // Configure basePath for GitHub Pages
   basePath: process.env.GITHUB_PAGES ? '/korean-flashcards' : '',
   assetPrefix: process.env.GITHUB_PAGES ? '/korean-flashcards/' : '',
